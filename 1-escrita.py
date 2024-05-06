956f44b77beab5a20f080960e02989bacc1afebf
alvesdesouzabruno <151651480+alvesbrunoo@users.noreply.github.com>
2024-05-06 18:35:36 -0300

    manipulação de arquivos

---
+++ 1-escrita.py
@@ -0,0 +1,15 @@
+name = input('Digite seu nome:\n')
+
+"""
+- Arquivos:
+1 - opção w - write
+2 - opção a - append
+3 - opção r - read
+"""
+#Alternativa 1
+# file = open('names.txt', 'a')
+# file.write(f'{name}\n')
+# file.close()
+
+with open('names.txt', 'r') as file:
+    file.write(f'{name}\n')
